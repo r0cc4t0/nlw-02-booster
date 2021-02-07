a12aa45dab8c64030c7aded3fb322d75fe64445e
r0cc4t0 <guilherme.roccato@outlook.com>
2021-02-07 19:24:44 -0300

    Concepts: div#root, JSX, Components

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import './App.css';
+import Header from './Header';
 
 function App() {
   return (
-    <h1>Hello, World!</h1>
+    <Header />
   );
 }
 
---
+++ web/src/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <p>Hello, World!</p>
+      <button type="button">Click here!</button>
+    </header>
+  );
+};
+
+export default Header;

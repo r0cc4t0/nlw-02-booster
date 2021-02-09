bb3eb8e422d00a45f33b3999e5a79879c1eab608
r0cc4t0 <guilherme.roccato@outlook.com>
2021-02-08 22:15:43 -0300

    Web Application Landing Page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import './assets/styles/global.css';
+import Landing from './pages/Landing';
 
 function App() {
   return (
-    <header>
-      <h1>Hello, World!</h1>
-      <button type="button">Click here!</button>
-    </header>
+    <Landing />
   );
 }
 

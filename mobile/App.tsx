bda5f6e44e62716f350de7cf23a0246714e18512
r0cc4t0 <guilherme.roccato@outlook.com>
2021-02-26 23:56:02 -0300

    Mobile Application Landing Page

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,26 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import AppLoading from 'expo-app-loading';
+import { Archivo_400Regular, Archivo_700Bold, useFonts } from '@expo-google-fonts/archivo';
+import { Poppins_400Regular, Poppins_600SemiBold } from '@expo-google-fonts/poppins';
+import Landing from './src/pages/Landing';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Hello, World!</Text>
-      <StatusBar style="light" />
-    </View>
-  );
-}
+  let [fontsLoaded] = useFonts({
+    Archivo_400Regular, Archivo_700Bold,
+    Poppins_400Regular, Poppins_600SemiBold
+  });
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#333',
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  title: {
-    color: '#fff',
-    fontSize: 32,
-    fontWeight: 'bold'
+  if (!fontsLoaded) {
+    return (
+      <AppLoading />
+    );
+  } else {
+    return (
+      <>
+        <Landing />
+        <StatusBar style="light" />
+      </>
+    );
   }
-});
+}

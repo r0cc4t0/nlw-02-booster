6f35eee8cfff48541439dc1e87a57c233a6fcfa1
r0cc4t0 <guilherme.roccato@outlook.com>
2021-03-02 22:35:04 -0300

    Mobile Application Teacher Item Component

--- mobile/src/pages/Favorites/index.tsx
+++ mobile/src/pages/Favorites/index.tsx
@@ -1,12 +1,27 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, ScrollView } from 'react-native';
 import styles from './styles';
 import PageHeader from '../../components/PageHeader';
+import TeacherItem from '../../components/TeacherItem';
 
 const Favorites = () => {
   return (
     <View style={styles.container}>
-      <PageHeader title="Proffys Disponíveis" />
+      <PageHeader title="Meus Proffys Favoritos" />
+
+      <ScrollView
+        style={styles.teacherList}
+        contentContainerStyle={{
+          paddingHorizontal: 16,
+          paddingBottom: 16
+        }}
+      >
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+      </ScrollView>
     </View>
   );
 };
